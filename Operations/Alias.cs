abe4cf8ba0bf7097219e97574ecee22c566431b1
ornaras <ornaras.us@gmail.com>
2025-07-17 13:15:44 +0400

    Реализация блока AS

---
+++ Operations/Alias.cs
@@ -0,0 +1,32 @@
+﻿using RetailCorrector.Blueprint.Abstractions;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace RetailCorrector.Blueprint.Operations
+{
+    public class Alias : BlockBase
+    {
+        protected override string Header { get; } = "Именование";
+        internal override BlockRowBase[] Rows { get; }
+
+        public Alias(): base(150)
+        {
+            var input = new TextBox
+            {
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                Width = 130
+            };
+            Grid.SetColumnSpan(input, 3);
+
+            var @in = In("Исходное");
+
+            Rows = [
+                @in, Custom(input),
+                Out("Регистрация", () => $"({@in.Endpoint!.Value()}) AS {input.Text}"),
+                Out("Название", () => input.Text)
+            ];
+            Draw();
+        }
+    }
+}
