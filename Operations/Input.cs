a809887d89f5cafc02097d67d9ce4fa334ffc666
ornaras <ornaras.us@gmail.com>
2025-07-17 12:51:02 +0400

    Удаление избыточного свойства

--- Operations/Input.cs
+++ Operations/Input.cs
@@ -2,7 +2,6 @@
 using RetailCorrector.Blueprint.Parts.Rows;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 
 namespace RetailCorrector.Blueprint.Operations
 {
@@ -10,24 +9,22 @@ namespace RetailCorrector.Blueprint.Operations
     {
         protected override string Header { get; } = "Строка";
 
-        public string Value { get; set; } = "";
-
         internal override BlockRowBase[] Rows { get; }
 
         public Input(): base(133)
         {
-            var @out = new BlockRowOut("Вывод", this);
             var _input = new TextBox
             {
                 VerticalAlignment = VerticalAlignment.Center,
                 HorizontalAlignment = HorizontalAlignment.Center,
                 Width = 120
             };
-            _input.SetBinding(TextBlock.TextProperty, new Binding(nameof(@out.Value)) { Source = @out });
             Grid.SetColumnSpan(_input, 3);
-            var input = new BlockRowCustom(_input, this);
 
-            Rows = [input, @out];
+            Rows = [
+                new BlockRowCustom(_input, this),
+                new BlockRowOut("Вывод", this)
+            ];
             Draw();
         }
     }
