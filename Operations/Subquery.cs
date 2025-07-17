da03c3fbd1631003dc182d6ed73d8f27fa982d4a
ornaras <ornaras.us@gmail.com>
2025-07-17 16:06:22 +0400

    Исправление ширины блока "подзапрос"

--- Operations/Subquery.cs
+++ Operations/Subquery.cs
@@ -6,7 +6,7 @@ namespace RetailCorrector.Blueprint.Operations
     {
         protected override string Header { get; } = "Подзапрос";
 
-        public Subquery(): base(100, 2)
+        public Subquery(): base(120, 2)
         {
             var @in = AddInRow(0, "Запрос", Statements);
             AddOutRow(1, "Результат", () => $"({@in.Endpoint!.Value()})");
