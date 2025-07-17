4e2826156c4a2535836a6e5382de88171a756cc7
ornaras <ornaras.us@gmail.com>
2025-07-17 15:46:27 +0400

    Добавлен блок "Подзапрос"

---
+++ Operations/Subquery.cs
@@ -0,0 +1,16 @@
+﻿using RetailCorrector.Blueprint.Abstractions;
+
+namespace RetailCorrector.Blueprint.Operations
+{
+    public class Subquery : BlockBase
+    {
+        protected override string Header { get; } = "Подзапрос";
+
+        public Subquery(): base(100, 2)
+        {
+            var @in = AddInRow(0, "Запрос", typeof(StatementBlockBase));
+            AddOutRow(1, "Результат", () => $"({@in.Endpoint!.Value()})");
+            Draw();
+        }
+    }
+}
