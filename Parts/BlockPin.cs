6324a25cfd27ce9dc3b6c2caa9303f8696728a70
ornaras <ornaras.us@gmail.com>
2025-07-17 09:42:49 +0400

    Добавление пинов BlockPinout

---
+++ Parts/BlockPin.cs
@@ -0,0 +1,23 @@
+﻿using RetailCorrector.Blueprint.Abstractions;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace RetailCorrector.Blueprint.Parts
+{
+    internal class BlockPin : FrameworkElement
+    {
+        private readonly bool _isInput;
+        private readonly BlockPinoutBase _parent;
+
+        public BlockPin(bool input, BlockPinoutBase parent)
+        {
+            VerticalAlignment = VerticalAlignment.Center;
+            HorizontalAlignment = HorizontalAlignment.Center;
+            Height = Width = 10;
+            _isInput = input;
+            _parent = parent;
+            AddVisualChild(new Ellipse() { Stroke = Brushes.Black });
+        }
+    }
+}
